25f4e3b9087b887c769e6d99a70efde39ab46bd3
Realm <vijay.techessentia@gmail.com>
2024-07-22 16:16:22 +0530

    update responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children} <SpeedInsights />
+        {children} <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
