32566c1636676c5c3cb60f7e03c9078b240cae91
Realm <vijay.techessentia@gmail.com>
2024-07-24 13:08:36 +0530

    imge added

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const nextConfig = {
   images: {
     formats: ["image/webp", "image/avif", "image/jpeg", "image/png"],
-    loader: "default",
+
     remotePatterns: [
       {
         hostname: "websitedemos.net",
